9831093d8fb92c2bd2bc75603371403cec8a6584
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-13 17:48:06 +0100

    added event listeners for each header button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+import home from "./home.js";
+
+const buttons = document.querySelectorAll("header button");
+
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		console.log(button);
+	});
+});
