a29c25dd53cda1003b1c916a159e28447234cb06
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-13 17:55:17 +0100

    added ful logic for header tabs without switching functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import "./styles.css";
+
 import home from "./home.js";
+import menu from "./menu.js";
+import about from "./about.js";
 
 const buttons = document.querySelectorAll("header button");
+const content = document.querySelector("#content");
 
 buttons.forEach((button) => {
 	button.addEventListener("click", () => {
-		console.log(button);
+		buttons.forEach((button) => {
+			button.classList.remove("selected");
+		});
+		button.classList.add("selected");
+
+		const tab = button.textContent;
+
+		switch (tab) {
+			case "Home":
+				break;
+			case "Menu":
+				break;
+			case "About":
+				break;
+		}
 	});
 });
